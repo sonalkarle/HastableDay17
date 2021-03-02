c6aff6ea148665d9b3707a1f3e49c5bca7a772ce
sonal <sonalkarle01@gmail.com>
2021-03-02 06:40:10 +0530

    Find the frequency of word from the sentence

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -28,37 +28,7 @@ namespace DataStructure
             int frequency = LinkedHashMap.Get("to");
             Console.WriteLine(frequency);
 
-            ///<summary>
-            ///UC2:Find the frequency of word from the paragraph
-            /// </summary>
-
-            string Paragraph = "Paranoids are not " +
-              "paranoid because they are paranoid but " +
-              "because they keep putting themselves " +
-              "deliberately into paranoid avoidable situations";
-            string[] letters = Paragraph.ToLower().Split(" ");
-
            
-            foreach (string word in letters)
-            {
-                int value = LinkedHashMap.Get(word);
-                if (value == default)
-                {
-                    value = 1;
-                }
-                else value += 1;
-                LinkedHashMap.Add(word, value);
-            }
-            int frequency1 = LinkedHashMap.Get("paranoid");
-            Console.WriteLine(frequency1);
-            ///<summary>
-            ///UC3: Remove particular word from the paragraph
-            /// </summary>
-            LinkedHashMap.Remove("avoidable");
-            int frequency2= LinkedHashMap.Get("avoidable");
-        
-            Console.WriteLine(frequency2);
-
 
         }
     }
--- hashtabletestcase/UnitTest1.cs
+++ hashtabletestcase/UnitTest1.cs
@@ -35,59 +35,6 @@ namespace NUnitTestProject
             Console.WriteLine(LinkedHashMap);
             Assert.AreEqual(2, frequency);
         }
-        /// <summary>
-        /// Tc2: Find the frequency of particular word from the paragraph
-        /// </summary>
-        [Test]
-        public void GivenParagraph_WheneAddedToHashTable_ShouldReturnWordFrequency()
-        {
-            string Paragraph = "Paranoids are not " +
-               "paranoid because they are paranoid but " +
-               "because they keep putting themselves " +
-               "deliberately into paranoid avoidable situations";
-            string[] words = Paragraph.ToLower().Split(" ");
-
-            foreach (string word in words)
-            {
-                int value = LinkedHashMap.Get(word);
-                if (value == default)
-                {
-                    value = 1;
-                }
-                else value += 1;
-                LinkedHashMap.Add(word, value);
-            }
-            int frequency = LinkedHashMap.Get("paranoid");
-            Console.WriteLine(LinkedHashMap);
-            Assert.AreEqual(3, frequency);
-        }
-        /// <summary>
-        /// Tc3:Remove particular word from the Paragharph
-        /// </summary>
-        [Test]
-        public void GivenParagraph_WheneAWordRemoved_ShouldReturnWordFrequency()
-        {
-            string Paragraph = "Paranoids are not " +
-               "paranoid because they are paranoid but " +
-               "because they keep putting themselves " +
-               "deliberately into paranoid avoidable situations";
-            string[] words = Paragraph.ToLower().Split(" ");
-
-            foreach (string word in words)
-            {
-                int value = LinkedHashMap.Get(word);
-                if (value == default)
-                {
-                    value = 1;
-                }
-                else value += 1;
-                LinkedHashMap.Add(word, value);
-            }
-
-            LinkedHashMap.Remove("avoidable");
-            int frequency = LinkedHashMap.Get("avoidable");
-            Console.WriteLine(LinkedHashMap);
-            Assert.AreEqual(0, frequency);
-        }
+       
     }
 }
